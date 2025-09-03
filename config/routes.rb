f02cdf78b88e6ec55ad5ce639a9d6ab3d77875b3
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-09-03 12:24:41 +0900

    Fix Devise routing: wrap root route in devise_scope block

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  root 'devise/sessions#new'
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 
 
 
