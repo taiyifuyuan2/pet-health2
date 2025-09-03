32a6649482c9240610abb06c51ff38a2bc8d3f26
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-09-03 11:54:56 +0900

    Fix migration file: remove invalid character from AddBreedAndWeightToPets

--- db/migrate/20250902100118_add_breed_and_weight_to_pets.rb
+++ db/migrate/20250902100118_add_breed_and_weight_to_pets.rb
@@ -1,6 +1,6 @@
 class AddBreedAndWeightToPets < ActiveRecord::Migration[7.1]
   def change
-え    add_reference :pets, :breed, null: true, foreign_key: true
+    add_reference :pets, :breed, null: true, foreign_key: true
     add_column :pets, :weight_kg, :decimal
   end
 end
